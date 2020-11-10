84f6cf0e9c529a5612b6740e6b7b665a23ccb9a4
Bryan <bryan0137@gmail.com>
2020-11-10 00:59:12 -0800

    uploaded files

---
+++ State_Machines/ftokenizer.cpp
@@ -0,0 +1,67 @@
+#include "ftokenizer.h"
+#include <fstream>
+#include "token.h"
+#include "stokenizer.h"
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+
+using namespace std;
+
+
+FTokenizer::FTokenizer()
+{
+
+}
+FTokenizer::FTokenizer(string fname){
+
+    string line;
+        //had to add where the file is located in the main
+     ifstream _f (fname);
+      if (_f.is_open())
+      {
+        while ( getline(_f,line) )
+        {
+            //line adds to all the lines
+            //all lines is later used for stokenizer
+        all_line=all_line+line;
+
+        }
+        //this is to know our initial all_line length
+        // is it will be changing
+        all_line_length=all_line.length();
+        _stk.set_string(all_line);
+        int index=0;
+        for(int i=0;i<all_line.length();i++){
+            char a= all_line.at(i);
+            if(a==a){
+               index++;
+            }else{
+                if(index<=16){
+
+                    index=0;
+                }
+            }
+        }
+
+      }else{
+
+          cout << "Unable to open file";
+      }
+
+}
+
+bool FTokenizer:: more(){
+    //if the position isn't equal to the initial all_line length
+    if(_stk.get_pos()!=all_line_length){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
+
+
+
